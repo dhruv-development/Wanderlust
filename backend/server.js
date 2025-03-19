a88b9615b050f18c741b33124ec7056546b74c99
Dhruv Rajvanshi <100338544+Dhruv-Rajvanshi@users.noreply.github.com>
2025-03-19 16:26:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,12 @@ const port = PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: '*', // Or specify your frontend origin(s) here for production
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+}));
 app.use(cookieParser());
 app.use(compression());
 
