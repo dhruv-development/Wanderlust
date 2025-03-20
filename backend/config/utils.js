9e9ba87dab040ddfa6655d69cc98343dc0911a4d
Dhruv Rajvanshi <100338544+Dhruv-Rajvanshi@users.noreply.github.com>
2025-03-20 14:44:40 +0530

    Update utils.js

--- backend/config/utils.js
+++ backend/config/utils.js
@@ -3,6 +3,6 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
-// const REDIS_URL = process.env.REDIS_URL;
+const REDIS_URL = process.env.REDIS_URL
 
-export { MONGODB_URI, PORT};
+export { MONGODB_URI, PORT, REDIS_URL};
